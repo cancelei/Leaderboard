d7545f745f859a555e93997f2da100a350b6e64c
Glauber <glauber.bannwart@gmail.com>
2023-06-30 12:33:10 -0300

    UpdateJS Implementation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,79 @@
 // import _ from 'lodash';
 import './style.css';
 
-// function component() {
-//   const element = document.createElement('div');
+const gamename = [{ 
+	"name": "My cool new game" 
+}];
+const gameID = "AsHkmrIRRRVDeNZNfO3f";
 
-//   // Lodash, now imported by this script
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//   element.classList.add('hello');
+const scorelist = [];
+const playername = document.getElementById('playername');
+const playerscore = document.getElementById('playerscore');
+const scoresubmit = document.getElementById('scoresubmit');
 
-//   return element;
+scoresubmit.addEventListener('click', () => {
+  const name = playername.value;
+  const score = parseInt(playerscore.value, 10);
+  postScores(name, score);
+  playername.value = '';
+  playerscore.value = '';
+});
+
+async function getScores() {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/{$gameID}/scores/');
+  if (response.ok) {
+
+  const data = await response.json();
+  const ul = document.getElementById('list');
+  ul.innerHTML = '';
+  data.result.forEach((score) => {
+    const li = document.createElement('li');
+    li.innerHTML = `${score.user}: ${score.score}`;
+    ul.appendChild(li);
+  }
+
+  );} else {
+    console.log('Error');
+  }
+}
+
+async function postScores(name, score) {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/{$gameID}/scores/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ user: name, score }),
+  });
+  if (response.ok) {
+    // window.location.reload();
+    getScores();
+  }  else {
+    console.log('Error');
+  }
+  
+  const data = await response.json();
+  return data;
+}
+
+// function createScoreList() {
+
+//   const scoreList = document.getElementById('ul');
+
+//   scoreList.setAttribute('id', 'score-list');
+//   return scoreList;
 // }
 
-// document.body.appendChild(component());
+// function createScoreListItem(name, score) {
+//   const scoreListItem = document.createElement('li');
+//   scoreListItem.innerHTML = `${name}: ${score}`;
+//   return scoreListItem;
+// }
+
+// function createScoreListItems(scores) {
+//   const scoreListItems = [];
+//   scores.forEach((score) => {
+//     scoreListItems.push(createScoreListItem(score.user, score.score));
+//   });
+//   return scoreListItems;
+// }
