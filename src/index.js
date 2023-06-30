75175b9e61d382905ca2c08cd25f9bc7256e27c3
Glauber <glauber.bannwart@gmail.com>
2023-06-30 12:58:21 -0300

    Update for Linter Fix

--- src/index.js
+++ src/index.js
@@ -1,39 +1,24 @@
 // import _ from 'lodash';
 import './style.css';
 
-const gamename = [{ 
-	"name": "My cool new game" 
-}];
-const gameID = "AsHkmrIRRRVDeNZNfO3f";
-
-const scorelist = [];
+// const gameID = 'AsHkmrIRRRVDeNZNfO3f';
 const playername = document.getElementById('playername');
 const playerscore = document.getElementById('playerscore');
 const scoresubmit = document.getElementById('scoresubmit');
 
-scoresubmit.addEventListener('click', () => {
-  const name = playername.value;
-  const score = parseInt(playerscore.value, 10);
-  postScores(name, score);
-  playername.value = '';
-  playerscore.value = '';
-});
-
 async function getScores() {
   const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/{$gameID}/scores/');
   if (response.ok) {
-
-  const data = await response.json();
-  const ul = document.getElementById('list');
-  ul.innerHTML = '';
-  data.result.forEach((score) => {
-    const li = document.createElement('li');
-    li.innerHTML = `${score.user}: ${score.score}`;
-    ul.appendChild(li);
-  }
-
-  );} else {
-    console.log('Error');
+    const data = await response.json();
+    const ul = document.getElementById('list');
+    ul.innerHTML = '';
+    data.result.forEach((score) => {
+      const li = document.createElement('li');
+      li.innerHTML = `${score.user}: ${score.score}`;
+      ul.appendChild(li);
+    });
+  } else {
+    // console.log('Error');
   }
 }
 
@@ -46,15 +31,21 @@ async function postScores(name, score) {
     body: JSON.stringify({ user: name, score }),
   });
   if (response.ok) {
-    // window.location.reload();
     getScores();
-  }  else {
-    console.log('Error');
+  } else {
+    // console.log('Error');
   }
-  
   const data = await response.json();
   return data;
 }
+
+scoresubmit.addEventListener('click', () => {
+  const name = playername.value;
+  const score = parseInt(playerscore.value, 10);
+  postScores(name, score);
+  playername.value = '';
+  playerscore.value = '';
+});
 
 // function createScoreList() {
 
